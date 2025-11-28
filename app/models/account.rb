1d0351607f8c8874b81a5f0513db5e0a8e6ba272
Ryan Pham <phuc.pham.itf@gmail.com>
2025-11-28 21:55:50 +0800

    feat(account): impl balance logic

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,11 +1,15 @@
+# frozen_string_literal: true
+
 class Account < ApplicationRecord
   belongs_to :user
+  has_many :transactions_as_from, class_name: 'Transaction', foreign_key: 'from_account_id', dependent: :nullify
+  has_many :transactions_as_to, class_name: 'Transaction', foreign_key: 'to_account_id', dependent: :nullify
 
-  validates :currency, presence: true
+  validates :currency, presence: true, uniqueness: { scope: :user_id }
 
-  # TODO: Implement this method
-  # This method returns the current balance of an account currency
   def balance
-    raise NotImplementedError
+    incoming = transactions_as_to.sum(:amount)
+    outgoing = transactions_as_from.sum(:amount)
+    incoming - outgoing
   end
 end
