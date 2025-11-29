60bc797379234a8b87146facc0476fbfaf5cd00e
Ryan Pham <phuc.pham.itf@gmail.com>
2025-11-29 16:44:35 +0800

    feat: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users = FactoryBot.create_list(:user, 10)
+
+currencies = %w[BTC ETH USDC]
+users.each do |user|
+  currencies.each do |currency|
+    account = FactoryBot.create(:account, user:, currency:)
+    FactoryBot.create(:transaction, :deposit, user:, to_account: account, amount: 1000.0)
+  end
+end
